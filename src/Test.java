96fb609e1465bfc95a8d67a8f47e9302308e5b7a
myosotis <1274034280@qq.com>
2019-12-08 23:19:37 +0800

    Organize File Structure

--- src/Test.java
+++ src/Test.java
@@ -8,23 +8,6 @@ import java.io.File;
 import java.util.*;
 
 public class Test {
-
-    public JPanel CreateComponent() {
-        JPanel panel=new JPanel();
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("D");
-        root.add(new DefaultMutableTreeNode("E"));
-        JTree tree=new JTree(root);
-        panel.add(tree);
-        panel.setVisible(true);
-        return panel;
-    }
-
     public static void main(String[] args) {
-        JFrame frame=new JFrame("教师学历信息");
-        frame.setSize(330,300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new Test().CreateComponent());
-        frame.pack();
-        frame.setVisible(true);
     }
 }
